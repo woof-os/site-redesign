c6e3d28d248bdd9a973ab4901f6f98cb7120fc0e
ACuteWoof <sutharshanvithushan@gmail.com>
2022-02-16 12:26:50 +0530

    unwanted js

--- scripts/getDownloads.js
+++ scripts/getDownloads.js
@@ -1,6 +1,6 @@
 function getQtileDLs() {
   const xhr = new XMLHttpRequest();
-  const url = "https://api.github.com/repos/obsproject/obs-studio/releases";
+  const url = "https://api.github.com/repos/woof-os/isos-qtile/releases";
   xhr.open("GET", url, true);
   xhr.onload = function () {
     const data = JSON.parse(this.response);
@@ -19,8 +19,6 @@ function getQtileDLs() {
   };
   xhr.send();
 }
-
-getQtileDLs();
 
 function getXfceDLs() {
   const xhr = new XMLHttpRequest();
@@ -41,37 +39,5 @@ function getXfceDLs() {
   xhr.send();
 }
 
+getQtileDLs();
 getXfceDLs();
-
-// DOWNLOAD WOOF OS ISO DIRECTLY //
-
-function redirectQtile() {
-  const xhr = new XMLHttpRequest();
-  const url = "https://api.github.com/repos/woof-os/isos-qtile/releases";
-  xhr.open("GET", url, true);
-  xhr.onload = function () {
-    const data = JSON.parse(this.response);
-    for (let i in data) {
-      try {
-        window.location.href = data[i].assets[0].browser_download_url;
-      } catch (error) {}
-    }
-  };
-
-  xhr.send();
-}
-
-function redirectXfce4() {
-  const xhr = new XMLHttpRequest();
-  const url = "https://api.github.com/repos/woof-os/isos-xfce4/releases";
-  xhr.open("GET", url, true);
-  xhr.onload = function () {
-    const data = JSON.parse(this.response);
-    for (let i in data) {
-      try {
-        window.location.href = data[i].assets[0].browser_download_url;
-      } catch (error) {}
-    }
-  };
-  xhr.send();
-}
