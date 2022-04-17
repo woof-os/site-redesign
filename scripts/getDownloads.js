da4e7c0da819252f077953d05c8013c6acec359f
ACuteWoof <sutharshanvithushan@gmail.com>
2022-04-17 17:06:22 +0530

    i3

--- scripts/getDownloads.js
+++ scripts/getDownloads.js
@@ -20,6 +20,28 @@ function getQtileDLs() {
   xhr.send();
 }
 
+function geti3DLs() {
+  const xhr = new XMLHttpRequest();
+  const url = "https://api.github.com/repos/woof-os/isos-i3/releases";
+  xhr.open("GET", url, true);
+  xhr.onload = function () {
+    const data = JSON.parse(this.response);
+    let dls = 0;
+    const dlCount = document.getElementById("i3-dl-count");
+
+    for (let i in data) {
+      try {
+        let assets = data[i].assets;
+        for (x in assets) {
+          dls = assets[x].download_count + dls;
+        }
+        dlCount.innerHTML = dls;
+      } catch (error) {}
+    }
+  };
+  xhr.send();
+}
+
 function getXfceDLs() {
   const xhr = new XMLHttpRequest();
   const url = "https://api.github.com/repos/woof-os/isos-xfce4/releases";
@@ -41,3 +63,4 @@ function getXfceDLs() {
 
 getQtileDLs();
 getXfceDLs();
+geti3DLs();
